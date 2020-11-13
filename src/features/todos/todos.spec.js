b42836b42c88d99182671bfdee7ae8db18d93c2f
igor <igor.parkhomenko@yalantis.net>
2020-11-13 12:47:04 +0200

    implement addTodo Id's generation

--- src/features/todos/todos.spec.js
+++ src/features/todos/todos.spec.js
@@ -1,4 +1,4 @@
-import todos from './todos'
+import todos, { addTodo, toggleTodo } from './todosSlice'
 
 describe('todos reducer', () => {
   it('should handle initial state', () => {
@@ -8,9 +8,8 @@ describe('todos reducer', () => {
   it('should handle ADD_TODO', () => {
     expect(
       todos([], {
-        type: 'ADD_TODO',
-        text: 'Run the tests',
-        id: 0,
+        type: addTodo.type,
+        payload: { text: 'Run the tests', id: 0 },
       })
     ).toEqual([
       {
@@ -30,9 +29,11 @@ describe('todos reducer', () => {
           },
         ],
         {
-          type: 'ADD_TODO',
-          text: 'Use Redux',
-          id: 1,
+          type: addTodo.type,
+          payload: {
+            text: 'Use Redux',
+            id: 1,
+          },
         }
       )
     ).toEqual([
@@ -63,9 +64,11 @@ describe('todos reducer', () => {
           },
         ],
         {
-          type: 'ADD_TODO',
-          text: 'Fix the tests',
-          id: 2,
+          type: addTodo.type,
+          payload: {
+            text: 'Fix the tests',
+            id: 2,
+          },
         }
       )
     ).toEqual([
@@ -92,32 +95,41 @@ describe('todos reducer', () => {
       todos(
         [
           {
-            text: 'Run the tests',
-            completed: false,
-            id: 1,
-          },
-          {
             text: 'Use Redux',
             completed: false,
             id: 0,
           },
+          {
+            text: 'Run the tests',
+            completed: false,
+            id: 1,
+          },
         ],
         {
-          type: 'TOGGLE_TODO',
-          id: 1,
+          type: toggleTodo.type,
+          payload: 1,
         }
       )
     ).toEqual([
-      {
-        text: 'Run the tests',
-        completed: true,
-        id: 1,
-      },
       {
         text: 'Use Redux',
         completed: false,
         id: 0,
       },
+      {
+        text: 'Run the tests',
+        completed: true,
+        id: 1,
+      },
     ])
+  })
+})
+
+describe('add Todo ', () => {
+  it('should generate incrementing todo IDs', () => {
+    const action1 = addTodo('a')
+    const action2 = addTodo('b')
+    expect(action1.payload).toEqual({ text: 'a', id: 0 })
+    expect(action2.payload).toEqual({ text: 'b', id: 1 })
   })
 })
---
+++ src/features/todos/todosSlice.js
@@ -0,0 +1,32 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+let nextTodoId = 0
+
+const todosSlice = createSlice({
+  name: 'todos',
+  initialState: [],
+  reducers: {
+    addTodo: {
+      reducer(state, action) {
+        const { id, text } = action.payload
+        state.push({ id, text, completed: false })
+      },
+      prepare(text) {
+        return { payload: { id: nextTodoId++, text } }
+      },
+    },
+    toggleTodo(state, action) {
+      const todo = state.find((todo) => todo.id === action.payload)
+      if (todo) {
+        todo.completed = !todo.completed
+      }
+    },
+  },
+})
+
+console.log(todosSlice)
+console.log(todosSlice.actions)
+console.log(todosSlice.reducer)
+
+export const { addTodo, toggleTodo } = todosSlice.actions
+export default todosSlice.reducer
